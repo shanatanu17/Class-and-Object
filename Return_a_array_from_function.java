92464e1efdd0f07e7ad9cb1b4f36d00fc34c3435
shantanu17 <shantanushinde3292@gmail.com>
2025-09-18 15:41:45 +0530

    first commit

---
+++ Return_a_array_from_function.java
@@ -0,0 +1,78 @@
+class temp
+{
+  // this a[] takes the space in statck memory
+  int a[];
+
+
+  // these fucntions also get space in stack memory
+  void setval(int arr[])
+  {
+    a = arr;
+    a[3] = 1700;
+  }
+
+  
+  //this returns a base address of array , so we need to take it in array thats why the return type is int[].
+  int[] getval()
+  {
+    return a;
+  }
+
+
+
+}
+
+
+
+
+public class Return_a_array_from_function
+{
+   public static void main(String ss[])
+   { 
+      
+               // this line bcz of new keyword takes space in heap memory
+               // s[] ---> takes space in stack memory it is a class level.
+
+     int s[] = new int[]{1,2,3,4,5,6};
+
+     // here  , we create a object of class and make its a reference variable.
+     temp obj = new temp();
+
+     System.out.println("Print the array elements");
+     for(int i=0;i<s.length;i++)
+      {
+         System.out.print(s[i]+" ");
+      }
+
+
+
+     obj.setval(s);
+
+
+     // here , called the getval() and which returns a base address of array and for that new 
+     // need to store it in a array.
+
+
+     int ans[] = obj.getval();
+
+      System.out.println("Print the array elements after changes");
+     for(int i=0;i<ans.length;i++)
+      {
+         System.out.print(ans[i]+" ");
+      }
+
+      
+
+
+     
+
+
+     
+ 
+ 
+ 
+ 
+ 
+
+   }
+}
